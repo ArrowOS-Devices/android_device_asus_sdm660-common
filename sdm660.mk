dbd064c75c3c36d5117d9fa1a1ac28a9fa3acc95
Danny Baumann <dannybaumann@web.de>
2019-01-22 14:15:40 +0100

    sdm660-common: Add touchscreen gestures HIDL HAL
    Change-Id: I02380e387f01fe20f984d2e6daa095f2056c03f9
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -423,6 +423,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.asus_sdm660
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
