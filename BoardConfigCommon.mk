54ec3ca8324da23e45d5b1b27879ca3e2e73fa29
Sonal Singh <sonal.singh.19993@gmail.com>
2021-09-29 00:56:35 +0200

    [SQUASH] sdm660-common: Disable FBE
    Revert "sdm660-common: Drop FDE support entirely"
    
    This reverts commit 111c9af7f9f9efb62570d342844170b784b8960f.
    
    Revert "sdm660-common: Drop f2fs support on /data for now"
    
    This reverts commit 64c02164dc3311fa00c71f5eb60afe4b332c7cbd.
    
    Revert "sdm660-common: rootdir: fstab: Switch to FBE"
    
    This reverts commit 2f2b256ab83cd4fafe5a49732a62ee516caf75bf.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -58,6 +58,9 @@ TARGET_USES_GRALLOC4 := true
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
+# Encryption
+TARGET_HW_DISK_ENCRYPTION := true
+
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 
