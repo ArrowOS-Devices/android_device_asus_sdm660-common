30a79b59e2a7ff5a46b0bdb1624a2814220e0fe9
RONIN808 <shubhamshedage808@gmail.com>
2022-03-14 20:23:38 +0530

    sdm660-common: Update WFD from ddv_sprout
    * From ddv_sprout-m690-user-11-RKQ1.210607.001-00WW_3_490-release-keys

--- sdm660.mk
+++ sdm660.mk
@@ -112,10 +112,15 @@ PRODUCT_PACKAGES += \
     gralloc.sdm660 \
     hwcomposer.sdm660 \
     memtrack.sdm660 \
-    libdisplayconfig \
-    libtinyxml \
+    libdisplayconfig.qti \
+    libdisplayconfig.qti.vendor \
     libqdMetaData \
-    libqdMetaData.system
+    libqdMetaData.vendor \
+    libtinyxml \
+    libvulkan \
+    vendor.display.config@1.11.vendor \
+    vendor.display.config@2.0 \
+    vendor.display.config@2.0.vendor
 
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
@@ -286,7 +291,8 @@ PRODUCT_PACKAGES += \
     libOmxSwVencHevc \
     libOmxVdec \
     libOmxVenc \
-    libstagefrighthw
+    libstagefrighthw \
+    libstagefright_enc_common
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
