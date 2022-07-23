9481160c7e49d25bc7364f1a421f0b75b9e7422e
Sonal Singh <sonal.singh.19993@gmail.com>
2022-07-23 11:09:21 +0000

    sdm660-common: overlay: Switch to runtime resource overlays

--- sdm660.mk
+++ sdm660.mk
@@ -280,9 +280,16 @@ PRODUCT_PACKAGES += \
     libstagefrighthw
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+PRODUCT_PACKAGES += \
+   CarrierConfigOverlay \
+   CellBroadCastReceiverOverlay \
+   DialerOverlay \
+   FrameworksResOverlay \
+   SettingsOverlay \
+   SettingsProviderOverlay \
+   SystemUIOverlay \
+   TelephonyOverlay \
+   TouchGesturesOverlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
